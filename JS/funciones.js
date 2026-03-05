21611ab6e5344a9930aa9c59500a1fc34043b100
Fabian Daquilema <109368214+Kirbo97@users.noreply.github.com>
2026-03-05 15:20:35 -0500

    Add files via upload

--- JS/funciones.js
+++ JS/funciones.js
@@ -2,7 +2,7 @@
 function Scrollbutton(tip_bot) {
     var anchoVentana = window.innerWidth;
     var salto_movile=525;
-    var salto_comp=540;
+    var salto_comp=520;
         
     if (anchoVentana <= 980) { // desplasamiento para cuando es  el movile
         if (tip_bot === 'arriba') {
@@ -13,7 +13,7 @@ function Scrollbutton(tip_bot) {
             
     } else if (anchoVentana > 980) { // desplasamiento para cuando es la computadora
         if (tip_bot === 'arriba') {
-            document.getElementById("boxsort").scrollTop -= salto_comp;
+            document.getElementById("boxsort").scrollTop -= (salto_comp);
         } else if (tip_bot === 'abajo') {
             document.getElementById("boxsort").scrollTop += salto_comp;
         }   
