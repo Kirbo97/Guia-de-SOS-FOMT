06e3065b9dcf287e379f72eac286071f76e26848
Fabian Daquilema <109368214+Kirbo97@users.noreply.github.com>
2026-03-06 19:31:52 -0500

    Add files via upload

--- JS/funciones.js
+++ JS/funciones.js
@@ -1,22 +1,42 @@
 
-function Scrollbutton(tip_bot) {
+function Scroll_button_villager(tip_bot_1) {
     var anchoVentana = window.innerWidth;
-    var salto_movile=523;
-    var salto_comp=520;
+    var salto_movile1=525;
+    var salto_comp1=520;
         
     if (anchoVentana <= 980) { // desplasamiento para cuando es  el movile
-        if (tip_bot === 'arriba') {
-            document.getElementById("boxsort").scrollTop -= salto_movile;
-        } else if (tip_bot === 'abajo') {
-            document.getElementById("boxsort").scrollTop += salto_movile;
+        if (tip_bot_1 === 'arriba') {
+            document.getElementById("villager_box").scrollTop -= salto_movile1;
+        } else if (tip_bot_1 === 'abajo') {
+            document.getElementById("villager_box").scrollTop += salto_movile1;
         }
             
     } else if (anchoVentana > 980) { // desplasamiento para cuando es la computadora
-        if (tip_bot === 'arriba') {
-            document.getElementById("boxsort").scrollTop -= (salto_comp);
-        } else if (tip_bot === 'abajo') {
-            document.getElementById("boxsort").scrollTop += salto_comp;
+        if (tip_bot_1 === 'arriba') {
+            document.getElementById("villager_box").scrollTop -= (salto_comp1);
+        } else if (tip_bot_1 === 'abajo') {
+            document.getElementById("villager_box").scrollTop += salto_comp1;
         }   
     }
+}   
 
+function Scroll_button_marriage(tip_bot_2) {
+    var anchoVentana = window.innerWidth;
+    var salto_movile2=630;
+    var salto_comp2=520;
+        
+    if (anchoVentana <= 980) { // desplasamiento para cuando es  el movile
+        if (tip_bot_2 === 'arriba') {
+            document.getElementById("marriage_box").scrollTop -= salto_movile2;
+        } else if (tip_bot_2 === 'abajo') {
+            document.getElementById("marriage_box").scrollTop += salto_movile2;
+        }
+            
+    } else if (anchoVentana > 980) { // desplasamiento para cuando es la computadora
+        if (tip_bot_2 === 'arriba') {
+            document.getElementById("marriage_box").scrollTop -= (salto_comp2);
+        } else if (tip_bot_2 === 'abajo') {
+            document.getElementById("marriage_box").scrollTop += salto_comp2;
+        }   
+    }
 }   
