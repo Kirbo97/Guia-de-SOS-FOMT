27a9a5b03276a1c0768e72cb4a7c5f87c07cf868
Fabian Daquilema <109368214+Kirbo97@users.noreply.github.com>
2026-03-08 22:17:03 -0500

    Add files via upload

--- JS/funciones.js
+++ JS/funciones.js
@@ -40,3 +40,32 @@ function Scroll_button_marriage(tip_bot_2) {
         }   
     }
 }   
+
+function cambiarTemporada(num){
+    let total = 4;
+    let encabezado = document.getElementById("estacion");
+    let titulo = document.getElementById("tituloEstacion");
+
+    for(let i=0;i<=total;i++){ document.getElementById("calendario"+i).style.display="none"; }
+    document.getElementById("calendario"+num).style.display="block";
+
+    if(num==1){ // primavera 
+        encabezado.style.backgroundColor="#84f16e"; 
+        titulo.textContent="Calendario de Primavera";
+    } 
+
+    if(num==2){ // verano
+        encabezado.style.backgroundColor="#f2e88c";
+        titulo.textContent="Calendario de Verano"; 
+    }
+
+    if(num==3){  // otoño
+        encabezado.style.backgroundColor="#f7b05f"; 
+        titulo.textContent="Calendario de Otoño"; 
+    }
+
+    if(num==4){  // invierno
+        encabezado.style.backgroundColor="#7ed1f5"; 
+        titulo.textContent="Calendario de Invierno"; 
+    }
+}
