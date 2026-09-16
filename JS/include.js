8976c82b992b42a7d63befd0fb5de9ddeb3752a7
Fabian Daquilema <109368214+Kirbo97@users.noreply.github.com>
2026-09-16 14:13:14 -0500

    Delete JS directory

--- JS/include.js
+++
@@ -1,8 +0,0 @@
-async function loadHTML(id, file) {
-  const response = await fetch(file);
-  const data = await response.text();
-  document.getElementById(id).innerHTML = data;
-}
-
-loadHTML("nav", "/Guia-de-SOS-FOMT/Estructura/nav.html");
-loadHTML("footer", "/Guia-de-SOS-FOMT/Estructura/footer.html");
