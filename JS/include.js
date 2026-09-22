5c4ad27f35ff58c2082d06b9ebdf355c38860c41
Fabian Daquilema <109368214+Kirbo97@users.noreply.github.com>
2026-09-22 13:59:43 -0500

    Add files via upload

--- JS/include.js
+++ JS/include.js
@@ -1,27 +1,34 @@
-async function loadHTML(id, file) {
-    const response = await fetch(file);
-    const data = await response.text();
-    document.getElementById(id).innerHTML = data;
+async function loadHTML(id, file) { 
+    const response = await fetch(file); 
+    const data = await response.text(); 
+    document.getElementById(id).innerHTML = data; 
 } 
+
 
 async function cargarPagina() {
     await loadHTML("loading", "/Guia-de-SOS-FOMT/Estructura/carga.html");
-    await Promise.all([ 
-        loadHTML("nav", "/Guia-de-SOS-FOMT/Estructura/nav.html"), 
-        loadHTML("footer", "/Guia-de-SOS-FOMT/Estructura/footer.html") 
-    ]); 
+
+    await Promise.all([
+        loadHTML("nav", "/Guia-de-SOS-FOMT/Estructura/nav.html"),
+        loadHTML("footer", "/Guia-de-SOS-FOMT/Estructura/footer.html")
+    ]);
+
     const imagenes = document.images;
-    await Promise.all( 
-        Array.from(imagenes).map(function (imagen) { 
+
+    await Promise.all(
+        Array.from(imagenes).map(function (imagen) {
+
             if (imagen.complete) { return Promise.resolve(); }
-            return new Promise(function (resolve) { 
-                imagen.addEventListener("load", resolve); 
-                imagen.addEventListener("error", resolve); 
-            }); 
-        }) 
+
+            return new Promise(function (resolve) {
+                imagen.addEventListener("load", resolve);
+                imagen.addEventListener("error", resolve);
+            });
+        })
     );
-    
-    document.getElementById("loading-screen").style.display = "none";
-} 
-        
+
+    document.getElementById("loading").style.display = "none";
+}
+
+
 cargarPagina();
